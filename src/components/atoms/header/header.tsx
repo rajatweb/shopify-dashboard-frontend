25e76f26ed77607c3860446cc94c5141e8932b2b
rajatweb <rajatsharma.web@gmail.com>
2024-09-11 11:10:15 +0530

    header basic

---
+++ src/components/atoms/header/header.tsx
@@ -0,0 +1,12 @@
+import { ThemeToggle } from '@/components/layout/theme-toggle'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <ThemeToggle />
+    </div>
+  )
+}
+
+export default Header;
