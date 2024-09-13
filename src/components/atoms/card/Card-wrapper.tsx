74ff1aef690bcdbfe6e41f91c731ab38e08fe9eb
Akshay <akshay4444.ak@gmail.com>
2024-09-13 18:10:47 +0530

    sortcards updated

--- src/components/atoms/card/Card-wrapper.tsx
+++ src/components/atoms/card/Card-wrapper.tsx
@@ -1,11 +1,16 @@
+import React from "react";
+import { useDroppable } from "@dnd-kit/core";
 interface Props {
   children: React.ReactNode;
 }
 
 const CardWrapper: React.FC<Props> = ({ children }) => {
+  const { setNodeRef } = useDroppable({
+    id: "card-wrapper", // give it a unique ID
+  });
   return (
-    <div className="p-5">
-      <article className={`border-2 border-dashed border-[#a2a3a4] w-[352px]`}>
+    <div className="p-5" ref={setNodeRef}>
+      <article  className={`border-2 border-dashed border-[#a2a3a4] w-[352px]`}>
         {children}
       </article>
     </div>
